f522161c2e3256ab0f8263ae0005b017f409ee39
Lucas Monteiro <lmsupport@outlook.com>
2017-12-06 19:20:32 -0200

    Ok

---
+++ 01_Desenvolvimento/project/PIM/PIM.Web/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿namespace PIM.Web.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
